7872defcfa5e292cd438cd0701035b07e443db2d
noctoviawan <n.octoviawan@gmail.com>
2026-04-18 20:35:46 -0500

    Update run_all.py

--- src/run_all.py
+++ src/run_all.py
@@ -1,1 +1,15 @@
-# placeholder
+from train_xgboost import train_xgboost_model
+from train_gpr import train_gpr_model
+from shap_analysis import run_shap_analysis
+
+FEATURES = [
+    "delta_pct",
+    "LogNb_Ti_over_V_Zr",
+    "Nb_over_Nb+Zr",
+    "V_Zr"
+]
+
+if __name__ == "__main__":
+    train_xgboost_model(feature_cols=FEATURES)
+    train_gpr_model(feature_cols=FEATURES)
+    run_shap_analysis(feature_cols=FEATURES)
