87864344bb3db75d63a86a2d03e30dbb351e0866
noctoviawan <n.octoviawan@gmail.com>
2026-04-20 09:08:51 -0500

    Update train_gpr.py

--- src/train_gpr.py
+++ src/train_gpr.py
@@ -27,8 +27,10 @@ def save_actual_vs_pred(y_true, y_pred, out_path, title):
     plt.close()
 
 
-def train_gpr_model(feature_cols=None, target_col="H", test_size=0.2, random_state=42):
-    df = load_and_preprocess()
+def train_gpr_model(df=None, feature_cols=None, target_col="H", ...):
+
+    if df is None:
+        df = load_and_preprocess()
 
     if feature_cols is None:
         feature_cols = [
